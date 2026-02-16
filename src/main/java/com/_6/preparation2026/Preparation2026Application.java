fceb8ef19ae8e86b5b3fd96907e5d6219477d295
Sagar Das <sagardas@Sagars-MacBook-Pro.local>
2026-02-16 14:08:48 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/_6/preparation2026/Preparation2026Application.java
@@ -0,0 +1,13 @@
+package com._6.preparation2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Preparation2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Preparation2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/_6/preparation2026/Preparation2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.preparation2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Preparation2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
